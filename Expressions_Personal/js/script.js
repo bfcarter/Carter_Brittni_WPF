e656737493fc01d2892bb106956976e2b90d144f
bfcarter <bfcarter@fullsail.edu>
2014-09-11 18:01:06 -0400

    added wet or dry food var

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,11 @@
 alert("This is in the js file");
 
 // how much dog food will I need for a year?
+
+//Ask the user if their dog eats wet or dry food?
+var wetOrDry =prompt ("Do you feed your dog wet or dry food?")
 //how many cans of wet food do I use each day?
 var foodCans =prompt ("How many cans of dog food do you use each day?")
 console.log(foodCans)
-
+//calculate the amount of food
+var yearFood=foodCans
