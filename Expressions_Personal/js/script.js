5c687801f2b590529bcdfed1650ffb0e8fa42af1
bfcarter <bfcarter@fullsail.edu>
2014-09-11 19:12:41 -0400

    solved yearFood

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,11 +1,23 @@
-alert("This is in the js file");
+/*
+Brittni Carter
+9/11/14
+WPF Section 01
+ */
+alert("Welcome,let's find out how much dog food you will need for a year.");
+
 
 // how much dog food will I need for a year?
-
-//Ask the user if their dog eats wet or dry food?
-var wetOrDry =prompt ("Do you feed your dog wet or dry food?")
 //how many cans of wet food do I use each day?
-var foodCans =prompt ("How many cans of dog food do you use each day?")
-console.log(foodCans)
-//calculate the amount of food
-var yearFood=foodCans
+var foodCans =prompt ("How many cans of dog food do you use each day?");
+console.log(foodCans);
+
+//days in a year multiplied by the amount of food used each day
+var daysInYear = 365;
+
+var yearFood = daysInYear * foodCans;
+
+alert("you will need "+yearFood+" cans of food this year");
+
+
+
+
