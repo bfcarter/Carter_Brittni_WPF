27909d760f22c506d67ac3462bd1275b7fca1a02
bfcarter <bfcarter@fullsail.edu>
2014-09-11 17:47:53 -0400

    Created personal - how much dog food for a year

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,7 @@
+alert("This is in the js file");
+
+// how much dog food will I need for a year?
+//how many cans of wet food do I use each day?
+var foodCans =prompt ("How many cans of dog food do you use each day?")
+console.log(foodCans)
+
