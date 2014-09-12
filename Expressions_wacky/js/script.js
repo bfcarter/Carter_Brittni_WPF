44213a370057383f07b4952e23a3245d3ad66e94
bfcarter <bfcarter@fullsail.edu>
2014-09-11 21:53:20 -0400

    added total amount to the question

--- Expressions_wacky/js/script.js
+++ Expressions_wacky/js/script.js
@@ -3,11 +3,11 @@
  9/11/2014
  WPF Section 1
  */
- alert ("Hello, let's find out which girl has the most shoes");
-
+ alert ("Hello, let's find out which girl has the most shoes and how many there are total");
+alert ("There is one rack with 200 shoes, one with 234, one with 155, and one with 333")
 //there are four racks of shoes
-var shoeRacks = [200,234,155,333]
-
-var amountOfRacks =prompt ("How many shoe racks are there?")
-console.log (amountOfRacks)
-var prompt= 
+var shoeRacks = [200,234,155,333];
+var amountOfRacks = 4;
+//ask how many shoe racks there are
+var amountOfRacks =prompt ("How many shoe racks are there?");
+console.log (amountOfRacks);
