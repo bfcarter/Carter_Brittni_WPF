87b7aed6333151465bd1401c06677b8022fa36c5
bfcarter <bfcarter@fullsail.edu>
2014-09-11 22:23:56 -0400

    changed question

--- Expressions_wacky/js/script.js
+++ Expressions_wacky/js/script.js
@@ -3,20 +3,20 @@
  9/11/2014
  WPF Section 1
  */
-alert ("Hello, let's find out which girl has the most shoes and how many there are total");
+alert ("Hello, Do you have more pairs of shoes them 4 other people?");
 alert ("There is one rack with 200 shoes, one with 234, one with 155, and one with 333");
-//there are four racks of shoes
-var shoeRacks = [200,234,155,333];
-
+//Ask the user how many racks of shoes there are
+var amountOfRacks =prompt ("How many shoe racks are there?");
+console.log (Number= amountOfRacks);
+// there are four racks of shoes
 var numRacks = 4;
 console.log(numRacks);
+
+var shoeRacks = [200,234,155,333];
+
 
 var total = shoeRacks[0] +shoeRacks[1] +shoeRacks[2] +shoeRacks[3];
 console.log(total);
 
-alert ("200 + 234 + 155 + 333" = "+shoeTotal+");
-//ask how many shoe racks there are
-
-//multiple the number of racks by the total pairs for shoes.
-var amountOfRacks =prompt ("How many shoe racks are there?");
-console.log (Number= amountOfRacks);
+alert ("200 + 234 + 155 + 333 = "+total+"");
+console.log (total)
