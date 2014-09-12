198afcb07c1c20da0c26107ec7c610f8a7312629
bfcarter <bfcarter@fullsail.edu>
2014-09-11 20:46:12 -0400

    added prompts to industry expressions

---
+++ Expressions_industry/js/script.js
@@ -0,0 +1,18 @@
+/*
+Brittni Carter
+9/11/14
+WPF Section 1
+Industry Expressions
+ */
+
+alert ("How many new positions need Facebook ads today?");
+
+var todayRequest = Prompt ("How many requests for ads did you receive today?");
+console.log ("todayRequest");
+
+var yesterdayRequest = prompt("How many requests for ads did you receive yesterday?")
+console.log("yesterdayRequest")
+
+var previousRequests = prompt ("How many previous older requests do you have?");
+console.log ("previousRequests")
+
--- Expressions_wacky/js/script.js
+++ Expressions_wacky/js/script.js
@@ -1,1 +1,11 @@
-alert("This is in the js file");
+/*
+ Brittni Carter
+ 9/11/2014
+ WPF Section 1
+ */
+alert("how many students did it take to screw in the lightbulb?");
+//ask how many students are in your class
+var classStudents = prompt ("how many students are in your class?")
+console.log (classStudents)
+
+//ask 
