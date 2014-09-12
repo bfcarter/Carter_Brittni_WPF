90eb52e00c87e8e43d8aef3beb2d69a855a150d5
bfcarter <bfcarter@fullsail.edu>
2014-09-11 22:42:49 -0400

    added another alert at the end of personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,10 +7,14 @@ alert("Welcome,let's find out how much dog food you will need for a year.");
 
 
 // how much dog food will I need for a year?
+var yourAns = prompt ("How many cans of food do you think you use each year?");
+console.log (yourAns);
 //how many cans of wet food do I use each day?
 var foodCans =prompt ("How many cans of dog food do you use each day?");
 console.log(foodCans);
-
+//ask how many days are in a year
+var askDays =prompt ("How many days are in a year?");
+console.log(askDays);
 //days in a year multiplied by the amount of food used each day
 var daysInYear = 365;
 
@@ -18,6 +22,6 @@ var yearFood = daysInYear * foodCans;
 
 alert("you will need "+yearFood+" cans of food this year");
 
-
+alert("Were you correct?");
 
 
