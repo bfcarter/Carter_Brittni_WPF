cf306b2e2008910daadb761d364b53ca840088c5
bfcarter <bfcarter@fullsail.edu>
2014-09-11 21:12:46 -0400

    added number to change from text

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -7,14 +7,14 @@ Industry Expressions
 
 alert ("let's find out how many new positions need Facebook ads today!");
 //ask how many requests for ads were received today
-var todayRequest = Prompt ("How many requests for ads did you receive today?");
+var todayRequest =prompt ("How many requests for ads did you receive today?");
 console.log (todayRequest);
 //ask how many requests for ads were received yesterday
-var yesterdayRequest = prompt("How many requests for ads did you receive yesterday?");
+var yesterdayRequest =prompt("How many requests for ads did you receive yesterday?");
 console.log(yesterdayRequest);
-var previousRequests = prompt ("How many previous older requests do you have?");
+var previousRequests =prompt ("How many previous older requests do you have?");
 console.log (previousRequests);
 //add all of the requests together
-var newAds = todayRequest + yesterdayRequest + previousRequests;
+var newAds = Number(todayRequest) + Number(yesterdayRequest) + Number(previousRequests);
 //send alert to let them know how many need to be created
-Alert ("You have "+newAds+" that need to be created today");
+alert ("You have "+newAds+" that need to be created today");
