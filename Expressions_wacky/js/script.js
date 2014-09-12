92fcb9e4bddf68fb2d8d87d6f74bfbc996094053
bfcarter <bfcarter@fullsail.edu>
2014-09-11 22:11:05 -0400

    corrected numRack error

--- Expressions_wacky/js/script.js
+++ Expressions_wacky/js/script.js
@@ -3,11 +3,20 @@
  9/11/2014
  WPF Section 1
  */
- alert ("Hello, let's find out which girl has the most shoes and how many there are total");
-alert ("There is one rack with 200 shoes, one with 234, one with 155, and one with 333")
+alert ("Hello, let's find out which girl has the most shoes and how many there are total");
+alert ("There is one rack with 200 shoes, one with 234, one with 155, and one with 333");
 //there are four racks of shoes
 var shoeRacks = [200,234,155,333];
-var amountOfRacks = 4;
+
+var numRacks = 4;
+console.log(numRacks);
+
+var total = shoeRacks[0] +shoeRacks[1] +shoeRacks[2] +shoeRacks[3];
+console.log(total);
+
+alert ("200 + 234 + 155 + 333" = "+shoeTotal+");
 //ask how many shoe racks there are
+
+//multiple the number of racks by the total pairs for shoes.
 var amountOfRacks =prompt ("How many shoe racks are there?");
 console.log (Number= amountOfRacks);
