9cd05d81835ffbd37b527267c297d440793698b8
bfcarter <bfcarter@fullsail.edu>
2014-09-11 22:48:10 -0400

    removed spacing from wacky

--- Expressions_wacky/js/script.js
+++ Expressions_wacky/js/script.js
@@ -3,11 +3,11 @@
  9/11/2014
  WPF Section 1
  */
+
 alert ("Hello, Do you have more pairs of shoes then other people and what would the total amount be?");
 var youWin = prompt ("Do you think you will have the most pairs of shoes?");
 var yourShoes =prompt ("How many pairs of shoes do you have?");
 console.log(yourShoes);
-
 alert ("The other people have 200, 234, 155 and 333");
 //Ask the user how many racks of shoes there are
 var amountOfRacks =prompt ("How many other people are you being compared with?");
@@ -15,14 +15,9 @@ console.log (Number= amountOfRacks);
 // there are four racks of shoes
 var numRacks = 4;
 console.log(numRacks);
-
 var shoeRacks = [200,234,155,333];
-
-
 var total = shoeRacks[0] +shoeRacks[1] +shoeRacks[2] +shoeRacks[3] + yourShoes;
 console.log(total);
-
 alert ("all of the shoes together are "+total+"");
 console.log (total);
-
 alert ("Do you have the most shoes?");
