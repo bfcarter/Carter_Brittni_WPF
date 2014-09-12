e9396477e5408d75885cd91b598338877fd33c90
bfcarter <bfcarter@fullsail.edu>
2014-09-11 20:51:43 -0400

    last alert added to industry

--- Expressions_industry/js/script.js
+++ Expressions_industry/js/script.js
@@ -10,9 +10,12 @@ alert ("How many new positions need Facebook ads today?");
 var todayRequest = Prompt ("How many requests for ads did you receive today?");
 console.log ("todayRequest");
 
-var yesterdayRequest = prompt("How many requests for ads did you receive yesterday?")
+var yesterdayRequest = prompt("How many requests for ads did you receive yesterday?");
 console.log("yesterdayRequest")
 
 var previousRequests = prompt ("How many previous older requests do you have?");
 console.log ("previousRequests")
 
+var newAds = todayRequest + yesterdayRequest + previousRequests;
+
+Alert ("You have "+newAds+" that need to be created today")
