608f372f8a30ec004c74344c2b6be8fa62d66bff
bfcarter <bfcarter@fullsail.edu>
2014-09-18 07:50:03 -0400

    added daysOfDayCare

--- Conditionals_personal/js/script.js
+++ Conditionals_personal/js/script.js
@@ -7,4 +7,15 @@ Brittni Carter
 
 alert("Lets find out how much it will cost to drop off your dog to doggie daycare!");
 
-//
+//find out how many days the dog will need to be dropped off
+
+var daysOfDayCare = prompt ("How many days a week will you need to use the daycare?")
+//Must input a number
+while (isNaN(daysOfDayCare)){
+    daysOfDayCare = prompt ("Please enter a number");
+    while (daysOfDayCare ===""){
+        daysOfDayCare = prompt ("Please don't leave blank, enter the amount of days");
+    }
+}
+console.log(daysOfDayCare);
+
