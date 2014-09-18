2c05bcf76648839c386b46247086f9409e26020c
bfcarter <bfcarter@fullsail.edu>
2014-09-18 07:41:20 -0400

    added math to industry conditional

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -2,7 +2,7 @@
  Brittni Carter
  9/17/14
  WPF Section 1
- Industry Conditional - Staffing (marketing)
+ Industry Conditional - Social Media (marketing)
  */
 alert("Thank you for your time, please answer the following questions");
 //Which Social Media Channel is most popular for your age group?
@@ -15,25 +15,26 @@ console.log(userName);
 
 //Ask the user their age
 var userAge = prompt ("Please type in your age");
+//Must input a number
 while (isNaN(userAge)){
     userAge = prompt ("Please enter a number representing your age.");
     while (userAge ===""){
-        userName = prompt ("Please don't leave blank, enter your age")
+        userName = prompt ("Please don't leave blank, enter your age");
     }
 }
-
-//Must input a number
-
-Console.log(userAge);
+console.log(userAge);
 
 //age groups for the three social media channels
 if (userAge >= 0 && userAge <= 18){
-    userAge = prompt
+    userAge = alert(""+userName+" Instagram is the most popular social media channel for your age group");
 }
 
-//Ask when they are most active on social media
 
-var userActivity = prompt ("Which social media channel do you use most? Twitter, Facebook or Instagram");
+if (userAge >= 19 && userAge <= 29){
+    userAge = alert(""+userName+" Twitter is the most popular social media channel for your age group");
+}
 
-console.log(userActivity);
+if (userAge >= 30 && userAge <= 40){
+    userAge = alert(""+userName+" LinkedIn is the most popular social media channel for your age group");
+}
 
