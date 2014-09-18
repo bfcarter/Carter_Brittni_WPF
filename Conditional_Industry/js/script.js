5632c00f2589c7081b1c847b9d55d536e5d49003
bfcarter <bfcarter@fullsail.edu>
2014-09-18 07:17:05 -0400

    changed industry conditional

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -5,17 +5,25 @@
  Industry Conditional - Staffing (marketing)
  */
 alert("Thank you for your time, please answer the following questions");
-//When is the best time to post on social media?
+//Which Social Media Channel is most popular for your age group?
 
 var userName = prompt ("Please type in your name");
 while (userName ===""){
     userName = prompt ("Please type in your name. \n Cannot continue if left blank")
 }
-console.log(userName)
+console.log(userName);
+
+//Ask the user their age
+var userAge = prompt ("Please type in your age");
+
+Console.log(userAge);
+
+//age groups for the three social media channels
+
 
 //Ask when they are most active on social media
 
-var userActivity = prompt ("When are you must active on Social Media? Morning, Afternoon or evening")
+var userActivity = prompt ("Which social media channel do you use most? Twitter, Facebook or Instagram")
 
-console.log(userActivity)
+console.log(userActivity);
 
