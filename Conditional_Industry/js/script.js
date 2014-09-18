3a23b4cb6b414a9b49571899e5d86cd7e7885768
bfcarter <bfcarter@fullsail.edu>
2014-09-17 23:23:44 -0400

    added while info to conditional_industry

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -8,6 +8,14 @@ alert("Thank you for your time, please answer the following questions");
 //When is the best time to post on social media?
 
 var userName = prompt ("Please type in your name");
-
+while (userName ===""){
+    userName = prompt ("Please type in your name. \n Cannot continue if left blank")
+}
 console.log(userName)
+
+//Ask when they are most active on social media
+
+var userActivity = prompt ("When are you must active on Social Media? Morning, Afternoon or evening")
+
+console.log(userActivity)
 
