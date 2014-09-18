0bc72c0c4e6dc93f2188979871d09183a556615a
bfcarter <bfcarter@fullsail.edu>
2014-09-17 22:47:30 -0400

    created esther's fox box

--- Conditionals_personal/js/script.js
+++ Conditionals_personal/js/script.js
@@ -1,1 +1,12 @@
-alert("This is in the js file");
+/*
+Brittni Carter
+ 9/17/14
+ WPF Section 1
+ Personal Conditionals
+ */
+
+alert("Lets find out how many bully sticks are in Esther's Toy Box!");
+
+//Create Esther's Toy Box
+var estherToyBox = ["squeaky toy","bully stick","tennis ball","frisbee","rawhide"];
+
