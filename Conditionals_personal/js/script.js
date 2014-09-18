b81646c17d255baf094e60f87658f0d5d5c04b39
bfcarter <bfcarter@fullsail.edu>
2014-09-18 07:58:13 -0400

    added the cost for half and full day

--- Conditionals_personal/js/script.js
+++ Conditionals_personal/js/script.js
@@ -9,7 +9,7 @@ alert("Lets find out how much it will cost to drop off your dog to doggie daycar
 
 //find out how many days the dog will need to be dropped off
 
-var daysOfDayCare = prompt ("How many days a week will you need to use the daycare?")
+var daysOfDayCare = prompt ("How many days a week will you need to use the daycare?");
 //Must input a number
 while (isNaN(daysOfDayCare)){
     daysOfDayCare = prompt ("Please enter a number");
@@ -18,4 +18,17 @@ while (isNaN(daysOfDayCare)){
     }
 }
 console.log(daysOfDayCare);
+
+var hoursAtDayCare = prompt ("How many hours per day will you need to use the daycare?");
+//Must input a number
+while (isNaN(hoursAtDayCare)){
+    hoursAtDayCare = prompt ("Please enter a number");
+    while (hoursAtDayCare ===""){
+        hoursAtDayCare = prompt ("Please don't leave blank, enter the amount of days");
+    }
+}
+console.log(hoursAtDayCare);
+
+var costFullDay = 22
+var costHalfDay = 14
 
