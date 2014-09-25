a9a679c50ad15f7bcaafea50f050bc85fcc016cc
bfcarter <bfcarter@fullsail.edu>
2014-09-25 16:17:44 -0400

    changed professional to personal

--- Function_industry/js/script.js
+++ Function_industry/js/script.js
@@ -1,1 +1,31 @@
-alert("This is in the js file");
+/*
+ Brittni Carter
+ 9/25/14
+ WPF Section 1
+ Function_Industry
+ */
+alert("Let's find out the the click through rate for a facebook ad!");
+var clicks = prompt ("How many clicks did this ad receive?");
+//user must use a number for answer
+while (isNaN(clicks)){
+    clicks = prompt ("Please enter a number");
+    while (clicks ===""){
+        clicks = prompt ("Please don't leave blank, enter the amount of days");
+    }
+}
+var impressions = prompt ("How many impressions did your ad receive?");
+//user must use a number for answer
+while (isNaN(impressions)){
+    impressions = prompt ("Please enter a number");
+    while (impressions ===""){
+        impressions = prompt ("Please don't leave blank, enter the amount of days");
+    }
+}
+var total =calcCtr(clicks, impressions);
+
+function calcCtr (clicks, impressions){
+    var clickThroughRate = clicks / impressions;
+   
+}
+
+console.log (total);
