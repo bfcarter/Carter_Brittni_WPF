1af0014932c7fe6bc01dad54a8e814924f9e8298
bfcarter <bfcarter@fullsail.edu>
2014-09-25 21:47:18 -0400

    added comments

--- Function_wacky/js/script.js
+++ Function_wacky/js/script.js
@@ -5,14 +5,14 @@ alert ("Let's find out if our 3 items allow us to use our half off coupon!");
 var item1 = prompt("What is the cost for your first item?");
 //user must use a number for answer
 while (item1 ==="" || isNaN(item1)){
-
+//reprompt user
         item1 = prompt ("Please don't leave blank, enter cost of your first item");
 
 }
 var item2 = prompt("What is the cost for your Second item?");
 //user must use a number for answer
 while (item2 ==="" || isNaN(item2)){
-
+//reprompt user
         item2 = prompt ("Please don't leave blank, enter the cost of your second item");
 
 }
@@ -20,7 +20,7 @@ while (item2 ==="" || isNaN(item2)){
 var item3 = prompt("What is the cost for your third item?");
 //user must use a number for answer
 while (item3 ==="" || isNaN(item3)){
-
+//reprompt user
         item3 = prompt ("Please don't leave blank, enter the cost of your third item");
 }
 
