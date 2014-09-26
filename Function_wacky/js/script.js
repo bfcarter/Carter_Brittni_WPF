8230b693802ed5155bbd252fce1711b3e5e9fa3b
bfcarter <bfcarter@fullsail.edu>
2014-09-25 20:15:29 -0400

    making changes

--- Function_wacky/js/script.js
+++ Function_wacky/js/script.js
@@ -1,6 +1,6 @@
 
 
-alert ("Let's find out if our 4 items allow us to use our half off coupon!");
+alert ("Let's find out if our 3 items allow us to use our half off coupon!");
 
 var item1 = prompt("What is the cost for your first item?");
 //user must use a number for answer
@@ -15,10 +15,27 @@ var item2 = prompt("What is the cost for your Second item?");
 while (isNaN(item2)){
     item2 = prompt ("Please enter a number");
     while (item2 ===""){
-        item2 = prompt ("Please don't leave blank, enter the amount of days");
+        item2 = prompt ("Please don't leave blank, enter the cost of your second item");
     }
 }
 
-var useCoupon = function(item1, item2, item3, item4){
-    var discout = item1 + item2 +item3 + item4
+var item3 = prompt("What is the cost for your third item?");
+//user must use a number for answer
+while (isNaN(item3)){
+    item3 = prompt ("Please enter a number");
+    while (item3 ===""){
+        item3 = prompt ("Please don't leave blank, enter the cost of your third item");
+    }
 }
+
+var useCoupon = function(item1, item2, item3){
+    var itemTotal = (item1 + item2 + item3);
+};
+
+
+for (shopTotal = 100; shopTotal>100;shopTotal++){
+    console.log (shopTotal + 'means you can use your coupon');
+}
+
+
+var shopTotal = useCoupon(50,60,70);
