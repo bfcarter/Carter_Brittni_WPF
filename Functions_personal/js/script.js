0a83dd7781357e39f89950ae207a201aaeaa3b6a
bfcarter <bfcarter@fullsail.edu>
2014-09-25 21:49:52 -0400

    correcting while code

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -2,11 +2,9 @@ alert("This is in the js file");
 
 var yarnPrice = prompt("What is the cost of your yarn");
 //user must use a number for answer
-while (isNaN(yarnPrice)){
-    yarnPrice = prompt ("Please enter a number");
-    while (yarnPrice ===""){
+while (yarnPrice ==="" || isNaN (yarnPrice)){
+
         yarnPrice = prompt ("Please don't leave blank, enter the cost of your yarn");
-    }
 }
 function calcYarnSale(){
     var studentDiscount = .15 * yarnPrice;
