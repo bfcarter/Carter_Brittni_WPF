725d830838f12c51dbfacc7bbbe43b45600d1d9a
bfcarter <bfcarter@fullsail.edu>
2014-09-25 21:46:18 -0400

    correcting while code

--- Function_wacky/js/script.js
+++ Function_wacky/js/script.js
@@ -4,28 +4,24 @@ alert ("Let's find out if our 3 items allow us to use our half off coupon!");
 
 var item1 = prompt("What is the cost for your first item?");
 //user must use a number for answer
-while (isNaN(item1)){
-    item1 = prompt ("Please enter a number");
-    while (item1 ===""){
+while (item1 ==="" || isNaN(item1)){
+
         item1 = prompt ("Please don't leave blank, enter cost of your first item");
-    }
+
 }
 var item2 = prompt("What is the cost for your Second item?");
 //user must use a number for answer
-while (isNaN(item2)){
-    item2 = prompt ("Please enter a number");
-    while (item2 ===""){
+while (item2 ==="" || isNaN(item2)){
+
         item2 = prompt ("Please don't leave blank, enter the cost of your second item");
-    }
+
 }
 
 var item3 = prompt("What is the cost for your third item?");
 //user must use a number for answer
-while (isNaN(item3)){
-    item3 = prompt ("Please enter a number");
-    while (item3 ===""){
+while (item3 ==="" || isNaN(item3)){
+
         item3 = prompt ("Please don't leave blank, enter the cost of your third item");
-    }
 }
 
 var useCoupon = function(item1, item2, item3){
--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -1,8 +1,16 @@
 alert("This is in the js file");
-var yarnPrice = 7.99;
 
+var yarnPrice = prompt("What is the cost of your yarn");
+//user must use a number for answer
+while (isNaN(yarnPrice)){
+    yarnPrice = prompt ("Please enter a number");
+    while (yarnPrice ===""){
+        yarnPrice = prompt ("Please don't leave blank, enter the cost of your yarn");
+    }
+}
 function calcYarnSale(){
-    var studentDiscount;
-    studentDiscount = .15 * yarnPrice;
+    var studentDiscount = .15 * yarnPrice;
+    return studentDiscount;
 
 }
+calcYarnSale();
